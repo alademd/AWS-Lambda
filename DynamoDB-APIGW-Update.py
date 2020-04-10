60d41c942611f65925ce4335088f98749b531c89
alademd <56764783+alademd@users.noreply.github.com>
2020-04-10 10:11:25 -0400

    Update DynamoDB-APIGW-Update.py

--- DynamoDB-APIGW-Update.py
+++ DynamoDB-APIGW-Update.py
@@ -14,7 +14,6 @@ def respond(err, res=None):
         },
     }
 
-
 def lambda_handler(event, context):
     '''Demonstrates a simple HTTP endpoint using API Gateway. You have full
     access to the request and response payload, including headers and
